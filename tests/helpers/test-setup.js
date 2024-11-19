1d55269233763eae201bd20ff32f861ea4bc00c2
dicucristiann <dicucristian24@gmail.com>
2024-11-19 20:31:48 +0200

    add GitHub Actions CI/CD workflow

--- tests/helpers/test-setup.js
+++ tests/helpers/test-setup.js
@@ -1,17 +1,15 @@
 require('dotenv').config({ path: './.env' });
-
 const { exec } = require('child_process');
 
-const testFile = process.env.TEST_RUN || '';
-const showReport = process.env.SHOW_REPORT === 'true';
+const testFile = process.env.TEST_RUN || ''; // Will run all tests if empty
+const showReport = process.env.SHOW_REPORT === 'true' && process.env.CI !== 'true';
+const headed = process.env.HEADED === 'true' ? '--headed' : '';
 
 if (!testFile) {
-    console.error('Error: TEST_RUN is not defined in the .env file.');
-    process.exit(1);
+    console.log('TEST_RUN not specified. Running all tests...');
 }
 
-// Playwright test command
-exec(`npx playwright test ${testFile} --headed`, (error, stdout, stderr) => {
+exec(`npx playwright test ${testFile} ${headed}`, (error, stdout, stderr) => {
     if (error) {
         console.error(`Error running test: ${error.message}`);
         return;
@@ -20,9 +18,9 @@ exec(`npx playwright test ${testFile} --headed`, (error, stdout, stderr) => {
         console.error(`Test Error: ${stderr}`);
         return;
     }
-    console.log(stdout); // test results
+    console.log(stdout); // results
 
-    // Options show the report
+    // Will generate report if SHOW_REPORT=true
     if (showReport) {
         console.log('Generating Playwright report...');
         exec('npx playwright show-report', (reportError, reportStdout, reportStderr) => {
@@ -34,7 +32,7 @@ exec(`npx playwright test ${testFile} --headed`, (error, stdout, stderr) => {
                 console.error(`Report Error: ${reportStderr}`);
                 return;
             }
-            console.log(reportStdout); // report output
+            console.log(reportStdout); //Output Report
         });
     } else {
         console.log('SHOW_REPORT is set to false. Skipping report generation.');
